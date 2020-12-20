3b484349ee1c0046a7eb27c9e6dd6278768630c1
Bronson Zgeb <bronson.zgeb@gmail.com>
2020-12-20 11:08:15 -0500

    Initial commit

---
+++ Assets/Fsm.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Fsm
+{
+    public enum Step
+    {
+        Enter,
+        Update,
+        Exit
+    }
+
+    public delegate FsmState FsmState(Step step);
+
+    private FsmState _currentState;
+
+    public void Start(FsmState startState)
+    {
+        TransitionToState(startState);
+    }
+
+    public void Update()
+    {
+        FsmState nextState = _currentState(Step.Update);
+        if (nextState != null)
+        {
+            TransitionToState(nextState);
+        }
+    }
+
+    private void TransitionToState(FsmState state)
+    {
+        if (_currentState?.Invoke(Step.Exit) != null)
+        {
+            Debug.LogWarning("FsmStates returned from Exit are ignored");
+        }
+
+        _currentState = state;
+        FsmState nextState = _currentState?.Invoke(Step.Enter);
+
+        if (nextState != null)
+        {
+            TransitionToState(nextState);
+        }
+    }
+}
