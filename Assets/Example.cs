3af5343b3542dc5c02dea2d87faa404fde2585e7
Bronson Zgeb <bronson.zgeb@gmail.com>
2020-12-20 11:24:37 -0500

    Added an example script

---
+++ Assets/Example.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    Fsm _fsm;
+
+    void Start()
+    {
+        _fsm = new Fsm();
+        _fsm.Start(IdleState);
+    }
+
+    void Update()
+    {
+        _fsm.Update();
+    }
+
+    Fsm.FsmState IdleState(Fsm.Step step)
+    {
+        if (step == Fsm.Step.Enter)
+        {
+            //On Enter
+            Debug.Log($"On ${nameof(IdleState)} Enter");
+        }
+        else if (step == Fsm.Step.Update)
+        {
+            //On Update
+            Debug.Log($"On ${nameof(IdleState)} Update");
+
+            // On A press transition to OtherState
+            if (Input.GetKeyDown(KeyCode.A))
+                return OtherState;
+        }
+        else if (step == Fsm.Step.Exit)
+        {
+            //On Exit
+            Debug.Log($"On ${nameof(IdleState)} Exit");
+        }
+
+        return null;
+    }
+
+    Fsm.FsmState OtherState(Fsm.Step step)
+    {
+        if (step == Fsm.Step.Enter)
+        {
+            //On Enter
+            Debug.Log($"On ${nameof(OtherState)} Enter");
+        }
+        else if (step == Fsm.Step.Update)
+        {
+            //On Update
+            Debug.Log($"On ${nameof(OtherState)} Update");
+
+            //On A press transition to IdleState
+            if (Input.GetKeyDown(KeyCode.A))
+                return IdleState;
+        }
+        else if (step == Fsm.Step.Exit)
+        {
+            //On Exit
+            Debug.Log($"On ${nameof(OtherState)} Exit");
+        }
+
+        return null;
+    }
+}
